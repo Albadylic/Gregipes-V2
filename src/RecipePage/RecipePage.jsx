7521650e7b748ed28ed7dba1368792355981f67a
Gregor <gregorlthomson@sky.com>
2019-10-24 17:10:19 +0100

    Add recipe page data Relates #7

--- src/RecipePage/RecipePage.jsx
+++ src/RecipePage/RecipePage.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import data from "../model/data";
 
 const RecipePage = () => {
   return (
     <section>
-      <h2>Welcome Recipe</h2>
+      <h2>{data.recipes[0].title}</h2>
+      <img src={data.recipes[0].image} alt={data.recipes[0].title} />
+      <p>{data.recipes[0].description}</p>
+      <ul>
+        {data.recipes[0].ingredients.map(ingredient => (
+          <li>{ingredient}</li>
+        ))}
+      </ul>
+      <ol>
+        {data.recipes[0].method.map(step => (
+          <li>{step}</li>
+        ))}
+      </ol>
     </section>
   );
 };
--- src/Results/RecipeCard.jsx
+++ src/Results/RecipeCard.jsx
@@ -6,16 +6,6 @@ const RecipeCard = ({ title, description, image, ingredients, method }) => {
       <h3>{title}</h3>
       <img src={image} alt={title} />
       <p>{description}</p>
-      <ul>
-        {ingredients.map(ingredient => (
-          <li>{ingredient}</li>
-        ))}
-      </ul>
-      <ol>
-        {method.map(step => (
-          <li>{step}</li>
-        ))}
-      </ol>
     </article>
   );
 };
