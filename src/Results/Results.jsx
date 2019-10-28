dc4c946a68e5f3032ce46311604f94dff4aea946
Gregor <gregorlthomson@sky.com>
2019-10-28 10:19:29 +0000

    Add routing between recipe cards and pages

--- src/Results/Results.jsx
+++ src/Results/Results.jsx
@@ -6,11 +6,12 @@ import "./Results.css";
 const Results = () => {
   return (
     <section id="results__container">
-      <h2 id="results__title">Welcome Results</h2>
+      <h2 id="results__title">Results</h2>
       <section id="results__card-container">
         {data.recipes.map((recipe, index) => (
           <RecipeCard
             key={index}
+            recipeIndex={index}
             title={recipe.title}
             description={recipe.description}
             image={recipe.image}
