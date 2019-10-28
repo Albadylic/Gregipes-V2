80dabc86524487ca6313431f01e60b9848f5e8ff
Gregor <gregorlthomson@sky.com>
2019-10-28 12:03:13 +0000

    Add two more recipes Relates #11

--- src/model/data.js
+++ src/model/data.js
@@ -1,5 +1,4 @@
 import defaultFood from "../assets/default-food.png";
-import defaultMusic from "../assets/default-music.png";
 
 export default {
   recipes: [
@@ -34,8 +33,84 @@ export default {
     },
     {
       id: 1,
+      title: "Flatbreads",
+      image: defaultFood,
+      description:
+        "A slightly sweet flatbread that can be eaten with a wide variety of cuisines.",
+      ingredients: [
+        "125g Flour",
+        "1 tsp Dry Active Yeast",
+        "1 tsp Salt",
+        "1 tbsp Golden Syrup",
+        "2 tbsp Olive Oil",
+        "75ml warm water"
+      ],
+      method: [
+        "Mix flour, yeast and salt in a bowl before adding water, syrup and oil.",
+        "Stir with a wooden spoon until a shaggy mass of dough forms and then mix by hand.",
+        "Once completely combined, tip onto a floured work suface and need until smooth and shiny.",
+        "Let rise for around two hours on the counter or overnight in the fridge.",
+        "Knead for a few minutes then rool into two-inch rounds, allow to rest for another 30 minutes.",
+        "Alternatively, the dough can be separated into two and then stretched and tucked to give a bun shape.",
+        "Bake at 180C for 8-10 minutes.",
+        "Serve warm."
+      ]
+    },
+    {
+      id: 2,
+      title: "Garlic and Coriander Naan",
+      image: defaultFood,
+      description:
+        "Light and chewy naan. The garlic and coriander can be replaced with dessicated coconut and sultanas for a sweeter bread.",
+      ingredients: [
+        "300g Bread Flour",
+        "1/2 tsp Dry Active Yeast",
+        "1 tsp Salt",
+        "1/2 tsp Sugar",
+        "150ml Warm Water",
+        "2 tbsp Yoghurt",
+        "2 cloves of garlic",
+        "Handful of Coriander Leaves"
+      ],
+      method: [
+        "Finely chop coriander and make garlic paste.",
+        "Combine flour, yeast, salt, sugar, garlic and coriander in a large mixing bowl.",
+        "Make a well and pour in yoghurt and water.",
+        "Mix all ingredients, firstly with a wooden spook and then by hand.",
+        "Knead dough on a well floured surface for around five minutes. The dough will be wet so flouring is vital.",
+        "Let prove for one hour or until doubled in size",
+        "Knead again for five minutes and divide into eight portions.",
+        "Roll each portion into a circle with a rolling pin or by hand-stretching. Lifting the dough into the air by one edge will give a teardrop shape.",
+        "Fry in butter until fully risen and crispy."
+      ]
+    },
+    {
+      id: 1,
       title: "example two",
-      image: defaultMusic,
+      image: defaultFood,
+      description:
+        "lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum",
+      ingredients: [
+        "2ingr1",
+        "2ingr2",
+        "2ingr3",
+        "2ingr4",
+        "2ingr5",
+        "2ingr6",
+        "2ingr7"
+      ],
+      method: [
+        "Second recipe First, put the things in the pan",
+        " Second recipe Then cook them a little",
+        "Second recipe Mix some things together",
+        "Second recipe add a garnish",
+        "Second recipe and serve the dish with salad"
+      ]
+    },
+    {
+      id: 1,
+      title: "example two",
+      image: defaultFood,
       description:
         "lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum",
       ingredients: [
