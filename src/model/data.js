faa72e86ec208366579405d7638e4d8a43c6b725
Gregor <gregorlthomson@sky.com>
2019-10-28 11:42:07 +0000

    Add Focaccia recipe

--- src/model/data.js
+++ src/model/data.js
@@ -5,25 +5,31 @@ export default {
   recipes: [
     {
       id: 0,
-      title: "example one",
+      title: "Focaccia",
       image: defaultFood,
       description:
-        "lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum",
+        "A thick and fluffy focaccia that will benefit from slow fermentation.",
       ingredients: [
-        "ingr1",
-        "ingr2",
-        "ingr3",
-        "ingr4",
-        "ingr5",
-        "ingr6",
-        "ingr7"
+        "500g Strong Bread Flour",
+        "2 tsp Salt",
+        "7g Dry Active Yeast",
+        "2 tbsp Olive Oil",
+        "300ml Cold Water"
       ],
       method: [
-        "First, put the things in the pan",
-        "Then cook them a little",
-        "Mix some things together",
-        "add a garnish",
-        "and serve the dish with salad"
+        "Combine dry ingredients in a large bowl, stir and make a well in the centre",
+        "Add oil and then cold water and mix together, intially with a wood spoon and once more coherent then by hand",
+        "Knead on a well floured suface for five minutes",
+        "Stretch and tuck sides for five minutes",
+        "Knead for a further five minutes",
+        "Cover and let rest in a warm place until doubled in size",
+        "Stretch across an oiled baking sheet. If the dough does not reach the edges, cover for 15 minutes and then stretch again.",
+        "Allow to prove for another hour or overnight.",
+        "Using your fingers, press dimples into the surface of the bread.",
+        "Make a salt brine by combining a tablespoon of salt and roughly 100ml of cold water. Stir until the salt is dissolved.",
+        "Pour this brine across the surface of the bread and into the dimples.",
+        "Bake at 220C for roughly 20 minutes.",
+        "Allow to rest for at least one hour before cutting into the bread."
       ]
     },
     {
