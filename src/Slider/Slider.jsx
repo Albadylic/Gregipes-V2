bf7a6ad60f5c19a2c46644c85ae403c95c93822d
Gregor <gregorlthomson@sky.com>
2020-01-20 18:15:53 +0000

    Add notes for what to do next Relates #15

--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -6,9 +6,10 @@ import "./Slider.css";
 
 // **TODO**
 // Remove the array of Indexes if possible or otherwise simplify this
-// In order to add in keypress functionality, the Counter will need to be more reactive
-// Read up on what is happening in useRef
-// Simplify component so that we're not using indexArray[Counter()] but something simpler instead
+// Simplify component so that we're not using indexArray[visibleRecipe()] but something simpler instead
+// In order to add in PlayPause&&keypress functionality, the visibleRecipe will need to be more reactive
+
+// Create an array which contains the indexes of three random recipes
 
 const indexArray = [];
 while (indexArray.length < 3) {
@@ -18,19 +19,28 @@ while (indexArray.length < 3) {
   }
 }
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+// This function will show one recipe at a time and will shift the visible recipe at a fixed interval
+const VisibleRecipe = () => {
+  const [visibleIndex, setVisibleIndex] = useState(0);
+
+  // If play===true (i.e. if button is clicked)
+  // When an arrow key is pressed, this should be paused and reset
+  // I think that the logic inside (i.e. the if) could be used when the right arrow key is pressed also
+  // The logic can be reversed for a press of left
 
   useInterval(() => {
-    if (count < 2) {
-      setCount(count => count + 1);
+    if (visibleIndex < 2) {
+      setVisibleIndex(visibleIndex => visibleIndex + 1);
     } else {
-      setCount(0);
+      setVisibleIndex(0);
     }
   }, 4000);
 
-  return count;
+  return visibleIndex;
 };
+
+// This is a custom hook which will take a callback and delay.
+// The hook will run the callback over the interval
 
 const useInterval = (callback, delay) => {
   const savedCallback = useRef();
@@ -53,22 +63,35 @@ const useInterval = (callback, delay) => {
   }, [delay]);
 };
 
+// Add functions for move left and right.
+
+// Add event listeners inside a useEffect
+
+// Component
+
 const Slider = () => {
-  const recipeEndpoint = "/recipe/" + indexArray[Counter()];
+  const recipeEndpoint = "/recipe/" + indexArray[VisibleRecipe()];
   return (
-    <article id="slider__container">
+    <section id="slider__container">
       <Link to={recipeEndpoint}>
-        <h3 id="slider__title">{data.recipes[indexArray[Counter()]].title}</h3>
+        <h3 id="slider__title">
+          {data.recipes[indexArray[VisibleRecipe()]].title}
+        </h3>
       </Link>
       <img
-        src={data.recipes[indexArray[Counter()]].image}
-        alt={data.recipes[indexArray[Counter()]].title}
+        src={data.recipes[indexArray[VisibleRecipe()]].image}
+        alt={data.recipes[indexArray[VisibleRecipe()]].title}
         id="slider__image"
       />
       <p id="slider__description">
-        {data.recipes[indexArray[Counter()]].description}
+        {data.recipes[indexArray[VisibleRecipe()]].description}
       </p>
-    </article>
+      <article id="slider__buttons">
+        <button>Left</button>
+        <button>Play/Pause</button>
+        <button>Right</button>
+      </article>
+    </section>
   );
 };
 
